7c3274970cbc27e5138aad7d338bc5578f505598
Revenge5432 <93908291+KubaROC@users.noreply.github.com>
2022-06-27 10:45:39 +0200

    Login update

--- includes/validate.php
+++ includes/validate.php
@@ -13,22 +13,31 @@ if ($_SERVER["REQUEST_METHOD"]== "POST") {
       
     $username = test_input($_POST["username"]);
     $password = test_input($_POST["password"]);
-    $stmt = $conn->prepare("SELECT * FROM gebruikers WHERE username = :username AND admin = 1");
+    $stmt = $conn->prepare("SELECT * FROM gebruikers WHERE username = :username");
     $stmt->bindParam(':username', $username);
     $stmt->execute();
     $user = $stmt->fetch();
 
     if(($user['username'] == $username) && 
         ($user['password'] == $password) &&
-        ($admin <= '1')) {
+        ($user['admin'] == '1')) {
             session_start();
             $_SESSION["name"] = 'admin';
 
             header("Location: ../admin/admin.php");
             exit();
     }
+    elseif(($user['username'] == $username) && 
+        ($user['password'] == $password) &&
+        ($user['admin'] == '0')) {
+            session_start();
+            $_SESSION["name"] = 'gebruiker';
+
+            header("Location: ../account.php");
+            exit();
+    } 
     else {
-        header("Location: ../admin/adminLogin.php");
+        header("Location: ../accountlogin.php");
         exit();
     }
 }
